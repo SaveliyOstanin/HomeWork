ab42c4147df5367acea753c0c84e6d2e5f39960a
savel <saveliy23381@gmail.com>
2023-02-28 00:37:12 +0300

    HomeWork2

---
+++ src/main/java/homeWork2.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class homeWork2 {
+    public static void getSimleNumbers(int first, int last){
+
+        ArrayList<Integer> simpleNumbers = new ArrayList<Integer>();
+
+        int mark;
+        System.out.println("Простые числа от " + first + " до " + last);
+        for (int i = first; i <= last; i++){
+            mark = 0;
+            for(int j = 2; j < i; j++) {
+                if (i % j == 0 && mark == 0) {
+                    mark++;
+                }
+            }
+            if (mark == 0) {
+                System.out.print(i + " ");
+                simpleNumbers.add(i);
+            }
+
+        }
+        System.out.println("\nИщем счасливое простое число....");
+        System.out.println("Так вот же оно: " + simpleNumbers.get(5) + "!!!");
+    }
+}
